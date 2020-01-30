be82ce42943b6d1223f63c0a2fae62fab8e1a6c8
Thomas Diesler <tdiesler@redhat.com>
2020-01-30 11:43:35 +0100

    [resolves #13] Add an API to classify unseen instances

---
+++ update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VERSION=1.0.1-SNAPSHOT
+
+rm -rf index.html img
+cp docs/target/nessus-weka-docs-$VERSION-userguide.tar.gz .
+tar xzf nessus-weka-docs-$VERSION-userguide.tar.gz 
+rm nessus-weka-docs-$VERSION-userguide.tar.gz 
+git add --all
